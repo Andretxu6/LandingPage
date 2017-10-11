5e93da017615dc0405f2d7f70124c7c163c9386d
Andretxu6 <aespadamu16dw@ikzubirimanteo.com>
2017-10-11 11:36:32 +0200

    miau

---
+++ app/Http/Controllers/inputContact.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contact;
+
+class inputContact extends Controller
+{
+    public function inputData(Request $request){
+        $request->validate([
+            'email' => 'required_without:phone',
+            'phone' => 'numeric',
+            'text' => 'required',
+        ]);
+
+        $row = new Contact;
+        $row->email=$_POST['email'];
+        $row->phone=$_POST['phone'];
+        $row->message=$_POST['text'];
+        $row->save();
+
+        return(redirect('/'));
+    }
+}
